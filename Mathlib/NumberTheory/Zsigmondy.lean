2150cb568835cf56b4a2f7c6380d5e87460390b4
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-02-24 23:25:24 +0900

    Working..

--- Mathlib/NumberTheory/Zsigmondy.lean
+++ Mathlib/NumberTheory/Zsigmondy.lean
@@ -1,9 +1,26 @@
 import Mathlib.Data.Nat.Basic
 import Mathlib.Data.Nat.Prime
+import Mathlib.Data.Int.Basic
+import Mathlib.Data.Int.ModEq
 import Mathlib.Data.Polynomial.Basic
 import Mathlib.RingTheory.Polynomial.Cyclotomic.Basic
 
 -- Following proof from https://pommetatin.be/files/zsigmondy_en.pdf
+
+theorem helpful_lemma1
+    {R : Type*} [Ring R] : True := ⟨⟩
+
+theorem monomial_minus_one_double_root_mod_prime
+    {p : ℕ} [Fact p.Prime]
+    {n : ℕ} {a : ℤ} {f : Polynomial (ZMod p)}
+    (h : Polynomial.monomial n (1 : ZMod p) - 1 =
+          ((Polynomial.X : Polynomial (ZMod p)) - a) ^ 2 * f) :
+    p ∣ n := by
+  by_cases h₀ : a ≡ 0 [ZMOD p]
+  · simp only [Int.ModEq, EuclideanDomain.zero_mod, EuclideanDomain.mod_eq_zero] at h₀
+
+    sorry
+  · sorry
 
 theorem zsigmondy_theorem
     {a b : ℕ} (h₁ : a > b) (h₂ : Nat.Coprime a b) {n : ℕ} (h₃ : 1 ≤ n)
