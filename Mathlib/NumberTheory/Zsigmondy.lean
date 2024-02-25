c769f66117eb333e18c5e3414c5ba5cb114f4deb
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-02-25 23:26:45 +0900

    Working...

--- Mathlib/NumberTheory/Zsigmondy.lean
+++ Mathlib/NumberTheory/Zsigmondy.lean
@@ -8,14 +8,23 @@ import Mathlib.RingTheory.Polynomial.Cyclotomic.Basic
 -- Following proof from https://pommetatin.be/files/zsigmondy_en.pdf
 
 theorem helpful_lemma1
-    {R : Type*} [Ring R] : True := ⟨⟩
+    {R : Type*} [Semiring R] [Nontrivial R]
+    {m n : ℕ} (hn : n ≠ 0)
+    {r₁ r₂ r₃ : R} (hr₁ : r₁ ≠ 0) (hr₂ : r₂ ≠ 0) (hr₃ : r₃ ≠ 0):
+    Polynomial.C r₁ * Polynomial.monomial m (r₂ : R) + Polynomial.C r₃ ≠ 0 := by
+  sorry
 
 theorem monomial_minus_one_double_root_mod_prime
     {p : ℕ} [Fact p.Prime]
     {n : ℕ} {a : ℤ} {f : Polynomial (ZMod p)}
     (h : Polynomial.monomial n (1 : ZMod p) - 1 =
-          ((Polynomial.X : Polynomial (ZMod p)) - a) ^ 2 * f) :
+          ((Polynomial.monomial 1 (1 : ZMod p)) - a) ^ 2 * f) :
     p ∣ n := by
+  have h₀ : n = 0 ∨ ¬ ↑p ∣ a := by
+    rintro ⟨x, h'⟩
+    simp only [h', Int.cast_mul, Int.cast_ofNat, CharP.cast_eq_zero, zero_mul, sub_zero,
+      Polynomial.monomial_pow, one_mul, one_pow] at h
+    sorry
   by_cases h₀ : a ≡ 0 [ZMOD p]
   · simp only [Int.ModEq, EuclideanDomain.zero_mod, EuclideanDomain.mod_eq_zero] at h₀
 
