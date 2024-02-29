0fbe5739e2f1e8c4ae05470a8a823b131e6959a8
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2024-02-29 15:04:42 +0900

    Working...

--- Mathlib/NumberTheory/Zsigmondy.lean
+++ Mathlib/NumberTheory/Zsigmondy.lean
@@ -9,27 +9,11 @@ import Mathlib.RingTheory.Polynomial.Cyclotomic.Basic
 
 theorem helpful_lemma1
     {R : Type*} [Semiring R] [Nontrivial R]
-    {m n : ℕ} (hn : m ≠ 0)
-    {r₁ : R} (hr₁ : r₁ ≠ 0) {r₂ : R} (hr₂ : r₂ ≠ 0) {r₃ : R} :
-    Polynomial.monomial m r₁ + Polynomial.C r₂ ≠ Polynomial.monomial n r₃ := by
-  intro h
-  simp only [Polynomial.ext_iff, Polynomial.coeff_add] at h
-  have hₘ:= h m
-  simp only [Polynomial.coeff_monomial, ↓reduceIte, ne_eq, hn, not_false_eq_true,
-    Polynomial.coeff_C_ne_zero, add_zero] at hₘ
-  simp_all only [ne_eq, ite_eq_right_iff, not_forall, exists_prop, ite_true]
-  clear hₘ
-  have h₀ := h 0
-  simp only [Polynomial.coeff_monomial, hn, ↓reduceIte, Polynomial.coeff_C_zero, zero_add] at h₀
-  exact hr₂ h₀
-
-theorem helpful_lemma2
-    {R : Type*} [Semiring R] [Nontrivial R]
     {n₁ n₂ : ℕ} (hn : n₁ ≠ n₂) {n₃ : ℕ}
     {r₁ : R} (hr₁ : r₁ ≠ 0) {r₂ : R} (hr₂ : r₂ ≠ 0) {r₃ : R} :
     Polynomial.monomial n₁ r₁ + Polynomial.monomial n₂ r₂ ≠ Polynomial.monomial n₃ r₃ := by
   intro h
-  simp [Polynomial.ext_iff] at h
+  simp only [Polynomial.ext_iff, Polynomial.coeff_add] at h
   have hn₁ := h n₁
   simp only [Polynomial.coeff_monomial, ↓reduceIte] at hn₁
   rw [ite_cond_eq_false r₂ 0 (by
@@ -39,17 +23,35 @@ theorem helpful_lemma2
   simp only [Polynomial.coeff_monomial, hn, ↓reduceIte, zero_add] at hn₂
   exact hr₂ hn₂
 
+theorem helpful_lemma2
+    {R : Type*} [Semiring R] [Nontrivial R] {f g: Polynomial R} {r : R} (hr : r ≠ 0) :
+    f * Polynomial.X + Polynomial.C r ≠ g * Polynomial.X := by
+  intro h
+  simp only [Polynomial.ext_iff, Polynomial.coeff_add] at h
+  have h₁ := h 0
+  simp only [Polynomial.mul_coeff_zero, Polynomial.coeff_X_zero, mul_zero, Polynomial.coeff_C_zero,
+    zero_add] at h₁
+  exact hr h₁
+
 theorem monomial_minus_one_double_root_mod_prime
     {p : ℕ} [Fact p.Prime]
-    {n : ℕ} {a : ℤ} {f : Polynomial (ZMod p)}
-    (h : Polynomial.monomial n (1 : ZMod p) - 1 =
-          ((Polynomial.monomial 1 (1 : ZMod p)) - a) ^ 2 * f) :
+    {n : ℕ} (h₀ : n ≠ 0)
+    {a : ℤ} {f : Polynomial (ZMod p)}
+    (h₁ : Polynomial.monomial n (1 : ZMod p) - 1 =
+          f * ((Polynomial.monomial 1 (1 : ZMod p)) - a) ^ 2) :
     p ∣ n := by
-  have h₀ : n = 0 ∨ ¬ ↑p ∣ a := by
+  have h₀ : ¬ ↑p ∣ a := by
     rintro ⟨x, h'⟩
     simp only [h', Int.cast_mul, Int.cast_ofNat, CharP.cast_eq_zero, zero_mul, sub_zero,
-      Polynomial.monomial_pow, one_mul, one_pow] at h
-    simp [helpful_lemma1]
+      Polynomial.monomial_pow, one_mul, one_pow] at h₁
+    have h₂ := Polynomial.monomial_mul_monomial 1 1 (1 : ZMod p) (1 : ZMod p)
+    simp only [Nat.reduceAdd, mul_one] at h₂
+    rw [← h₂, ← mul_assoc] at h₁
+    have := @helpful_lemma2 (ZMod p) _ _ (Polynomial.monomial n 1) (f * (Polynomial.monomial 1 1)) (p - 1)
+      (by simp only [CharP.cast_eq_zero, zero_sub, ne_eq, neg_eq_zero, one_ne_zero,
+        not_false_eq_true])
+    apply this
+    rw [← Polynomial.monomial_one_one_eq_X]
     sorry
   by_cases h₀ : a ≡ 0 [ZMOD p]
   · simp only [Int.ModEq, EuclideanDomain.zero_mod, EuclideanDomain.mod_eq_zero] at h₀
