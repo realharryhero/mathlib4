e0292d21b7d13d37b33f6503f3b4cca59b2ca2c5
Yaël Dillies <yael.dillies@gmail.com>
2024-03-17 16:38:15 +0000

    remove dead file

--- Mathlib/MeasureTheory/Integral/BoundedContinuousFunction.lean
+++ Mathlib/MeasureTheory/Integral/BoundedContinuousFunction.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kalle Kytölä
 -/
 import Mathlib.MeasureTheory.Integral.Bochner
-import Mathlib.Topology.Order.Bounded
 
 /-!
 # Integration of bounded continuous functions
