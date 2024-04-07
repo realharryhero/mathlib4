21ee63094fd1e33183f3c9fa4abc3295d3eb8b4b
Parcly Taxel <reddeloostw@gmail.com>
2024-04-07 18:52:31 +0800

    An extra import

--- Mathlib/Combinatorics/SimpleGraph/Turan.lean
+++ Mathlib/Combinatorics/SimpleGraph/Turan.lean
@@ -3,6 +3,7 @@ Copyright (c) 2024 Jeremy Tan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Tan
 -/
+import Mathlib.Algebra.BigOperators.Ring
 import Mathlib.Combinatorics.SimpleGraph.Clique
 import Mathlib.Data.Int.CardIntervalMod
 import Mathlib.Order.Partition.Equipartition
