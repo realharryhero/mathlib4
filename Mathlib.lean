3e25f5708746c5c53a419cb6350dc29a7a79ede5
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2024-04-20 22:47:15 +0200

    wip 3

--- Mathlib.lean
+++ Mathlib.lean
@@ -380,6 +380,7 @@ import Mathlib.Algebra.Module.Prod
 import Mathlib.Algebra.Module.Projective
 import Mathlib.Algebra.Module.Submodule.Basic
 import Mathlib.Algebra.Module.Submodule.Bilinear
+import Mathlib.Algebra.Module.Submodule.Equiv
 import Mathlib.Algebra.Module.Submodule.Ker
 import Mathlib.Algebra.Module.Submodule.Lattice
 import Mathlib.Algebra.Module.Submodule.LinearMap
