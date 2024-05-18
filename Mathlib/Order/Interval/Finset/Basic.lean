e2a21d3fa150873f3f618f2f118dece8b3f5b4c8
Yaël Dillies <yael.dillies@gmail.com>
2024-05-18 06:49:50 +0000

    update import

--- Mathlib/Order/Interval/Finset/Basic.lean
+++ Mathlib/Order/Interval/Finset/Basic.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Scott Morrison, Yaël Dillies
 -/
 import Mathlib.Algebra.Group.Embedding
-import Mathlib.Data.Set.Intervals.Monoid
+import Mathlib.Algebra.Order.Interval.Set.Monoid
 import Mathlib.Order.Cover
 import Mathlib.Order.Interval.Finset.Defs
 
